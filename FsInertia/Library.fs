b302899c7614e544f83b20fa1a9da6763751e755
Bryan Macrie <bdmacrie@gmail.com>
2023-11-28 14:52:08 -0600

    clean up

--- FsInertia/Library.fs
+++ FsInertia/Library.fs
@@ -43,32 +43,6 @@ type Page =
     member x.toJson () =
         JsonSerializer.Serialize<Page>(x)
 
-module Dynamic =
-    open System
-    open FSharp.Reflection
-
-    type InvokeResult = 
-        | Success of obj
-        | ObjectWasNotAFunction of Type
-
-    let dynamicFunction (fn:obj) (args:obj seq) =
-        let rec dynamicFunctionInternal (next:obj) (args:obj list) : InvokeResult =
-            match args.IsEmpty with
-            | false ->
-                let fType = next.GetType()
-                if FSharpType.IsFunction fType then
-                    let (head, tail) = (args.Head, args.Tail)
-                    let methodInfo = 
-                        fType.GetMethods()
-                        |> Seq.filter (fun x -> x.Name = "Invoke" && x.GetParameters().Length = 1)
-                        |> Seq.head
-                    let partalResult = methodInfo.Invoke(next, [| head |])
-                    dynamicFunctionInternal partalResult tail
-                else ObjectWasNotAFunction fType
-            | true ->
-                Success(next)
-        dynamicFunctionInternal fn (args |> List.ofSeq )
-
 module Handlers =
 
     open FSharpx.Collections
